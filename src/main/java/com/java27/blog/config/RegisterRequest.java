d4f70489af2f31b2b2abd65bd43488c258a39ed2
DonatoAletta <donatoalettawork@gmail.com>
2024-11-12 13:47:53 +0100

    Added RegisterRequest

---
+++ src/main/java/com/java27/blog/config/RegisterRequest.java
@@ -0,0 +1,18 @@
+package com.java27.blog.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+
+    private String name;
+    private String surname;
+    private String email;
+    private String password;
+}
