eaf598abcaed5eef99d72eb2b21f4a27823b259e
DonatoAletta <donatoalettawork@gmail.com>
2024-11-12 13:46:14 +0100

    Added AuthenticationController

---
+++ src/main/java/com/java27/blog/controller/AuthenticationController.java
@@ -0,0 +1,31 @@
+package com.java27.blog.controller;
+
+import com.java27.blog.config.AuthenticationResponse;
+import com.java27.blog.config.LoginRequest;
+import com.java27.blog.config.RegisterRequest;
+import com.java27.blog.dto.UserDTO;
+import com.java27.blog.service.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/register-user")
+    public ResponseEntity<UserDTO> registerUser(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authenticationService.registerUser(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> login(@RequestBody LoginRequest request) throws Exception {
+        return ResponseEntity.ok(authenticationService.login(request));
+    }
+
+}
