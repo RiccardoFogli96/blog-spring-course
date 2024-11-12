9347628c73ea52ba92fe2d6d0ed3732cb370a1bc
DonatoAletta <donatoalettawork@gmail.com>
2024-11-12 13:46:33 +0100

    Added AuthenticationResponse

---
+++ src/main/java/com/java27/blog/config/AuthenticationResponse.java
@@ -0,0 +1,14 @@
+package com.java27.blog.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AuthenticationResponse {
+    private String token;
+}
