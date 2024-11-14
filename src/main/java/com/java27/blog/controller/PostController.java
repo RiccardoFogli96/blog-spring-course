65fa714efac529f3d9ebfb426124fdaf767cc6aa
Simone Cuccu <simone.cuccu3@gmail.com>
2024-11-14 12:48:57 +0100

    add post

---
+++ src/main/java/com/java27/blog/controller/PostController.java
@@ -0,0 +1,24 @@
+package com.java27.blog.controller;
+
+import com.java27.blog.dto.CreatePostDTO;
+import com.java27.blog.dto.PostDTO;
+import com.java27.blog.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class PostController {
+
+    private final PostService postService;
+
+    @PostMapping
+    public ResponseEntity<PostDTO> addNewPost (@AuthenticationPrincipal UserDetails userDetails, @RequestBody CreatePostDTO createPostDTO){
+        return ResponseEntity.ok(postService.addNewPost(userDetails, createPostDTO));
+    }
+}
---
+++ src/main/java/com/java27/blog/mapper/PostMapper.java
@@ -0,0 +1,29 @@
+package com.java27.blog.mapper;
+
+import com.java27.blog.dto.CreatePostDTO;
+import com.java27.blog.dto.PostDTO;
+import com.java27.blog.entity.Post;
+import com.java27.blog.entity.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PostMapper {
+
+    public Post toPost (CreatePostDTO createPostDTO, User user){
+        return Post.builder()
+                .author(user)
+                .title(createPostDTO.getTitle())
+                .content(createPostDTO.getContent())
+                .createAt(createPostDTO.getCreateAt())
+                .build();
+    }
+
+    public PostDTO toPostDTO (Post post){
+        return PostDTO.builder()
+                .title(post.getTitle())
+                .content(post.getContent())
+                .createAt(post.getCreateAt())
+                .build();
+
+    }
+}
